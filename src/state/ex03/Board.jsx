220273d1dc7d4e28dc554bb8408bae2de552e5fb
MrGuizao <guigen131@gmail.com>
2020-02-03 12:06:54 -0300

    Light out terminado

--- src/state/ex03/Board.jsx
+++ src/state/ex03/Board.jsx
@@ -1,59 +1,68 @@
 import React, { Component } from "react";
 import "./board.css";
-import "./cell.css";
-
 
 import Cell from "./Cell";
 
 export default class Board extends Component {
      static defaultProps = {
-          nRows: 5,
-          nCols: 5,
-          changeLight: 0.25
+          nrows: 5,
+          ncols: 5,
+          chanceLightStartsOn: 0.25
      };
      constructor(props) {
           super(props);
+
           this.state = {
                hasWon: false,
                board: this.createBoard()
           };
      }
 
-     createBoard = () => {
+     createBoard() {
           let board = [];
-          for (let y = 0; y < this.props.nRows; y++) {
+          for (let y = 0; y < this.props.nrows; y++) {
                let row = [];
-               for (let x = 0; x < this.props.nCols; x++) {
-                    row.push(Math.random() < this.props.changeLight);
+               for (let x = 0; x < this.props.ncols; x++) {
+                    row.push(Math.random() < this.props.chanceLightStartsOn);
                }
                board.push(row);
           }
           return board;
-     };
+     }
 
-     flipCellAround = coord => {
-          let { nCols, nRows } = this.props;
+     flipCellsAround(coord) {
+          let { ncols, nrows } = this.props;
           let board = this.state.board;
           let [y, x] = coord.split("-").map(Number);
 
           function flipCell(y, x) {
-               if (x >= 0 && x < nCols && y >= 0 && y < nRows) {
+               if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
                     board[y][x] = !board[y][x];
                }
           }
-     };
+          flipCell(y, x);
+          flipCell(y, x - 1);
+          flipCell(y, x + 1);
+          flipCell(y - 1, x);
+          flipCell(y + 1, x);
 
-     render() {
+          let hasWon = board.every(row => row.every(cell => !cell));
+          this.setState({ board: board, hasWon: hasWon });
+     }
+
+     makeTable() {
           let tblBoard = [];
-          for (let y = 0; y < this.props.nRows; y++) {
+          for (let y = 0; y < this.props.nrows; y++) {
                let row = [];
-               for (let x = 0; x < this.props.nCols; x++) {
+               for (let x = 0; x < this.props.ncols; x++) {
                     let coord = `${y}-${x}`;
                     row.push(
                          <Cell
                               key={coord}
                               isLit={this.state.board[y][x]}
-                              flipCellAroundMe={() => this.flipCellAround(coord)}
+                              flipCellsAroundMe={() =>
+                                   this.flipCellsAround(coord)
+                              }
                          />
                     );
                }
@@ -63,11 +72,24 @@ export default class Board extends Component {
                <table className="Board">
                     <tbody>{tblBoard}</tbody>
                </table>
-
-               // <div class="container">
-               //      <div class="neon">Neon </div>
-               //      <div class="flux">Flux </div>
-               // </div>
+          );
+     }
+     render() {
+          return (
+               <div className="container">
+                    <h1 class="neon">LIGHTS </h1>
+                    <h1 class="flux">OUT </h1>
+                    {this.state.hasWon ? (
+                         <div className="winner">
+                              <span className="neon-orange">YOU</span>
+                              <span className="neon-blue">WIN!</span>
+                         </div>
+                    ) : (
+                         <div>
+                              {this.makeTable()}
+                         </div>
+                    )}
+               </div>
           );
      }
 }
--- src/state/ex03/Cell.jsx
+++ src/state/ex03/Cell.jsx
@@ -3,13 +3,13 @@ import React, { Component } from "react";
 export default class Cell extends Component {
      constructor(props) {
           super(props);
+          this.handleClick = this.handleClick.bind(this);
      }
-
-     handleClick = evt => {
-          this.props.flipCellAroundMe();
-     };
+     handleClick(evt) {
+          this.props.flipCellsAroundMe();
+     }
      render() {
-          let classes = "Cell" + (this.props.isLit ? "Cell-lit" : "");
+          let classes = "Cell" + (this.props.isLit ? " Cell-lit" : "");
           return <td className={classes} onClick={this.handleClick} />;
      }
 }
